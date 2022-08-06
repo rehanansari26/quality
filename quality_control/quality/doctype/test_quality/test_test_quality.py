2435a3a17a975746508ea29fd923e11734ef72d5
midhat@2020 <midhat@preciholesports.com>
2022-08-06 18:23:11 +0530

    Added my project

---
+++ quality_control/quality/doctype/test_quality/test_test_quality.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Rehan and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTestQuality(FrappeTestCase):
+	pass
---
+++ quality_control/quality/doctype/testing/testing.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Rehan and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Testing', {
+	// refresh: function(frm) {
+
+	// }
+});
